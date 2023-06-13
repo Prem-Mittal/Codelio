b6da797215c1d9d81405354e327a30b4cfdc7b5e
Prem <premmittal88@gmail.com>
2023-06-13 14:14:20 +0530

    Starting commit-1

---
+++ controllers/users_controller.js
@@ -0,0 +1,23 @@
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Codeial| SignUp'
+    })
+}
+
+
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Codeial|Sign In'
+    })
+}
+
+
+
