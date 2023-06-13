a3c195112070fc658fbdebb69d8be7019cd43bd9
Prem <premmittal88@gmail.com>
2023-06-13 18:31:14 +0530

    Commit-1 another branch

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,19 @@
 const User=require('../models/user')
+
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user:user
+            })
+        }
+        return res.redirect('back');
+    });
+}else{
+    return res.redirect('/users/sign-in');
+}
 }
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
@@ -38,7 +49,21 @@ module.exports.create=function(req,res){
     })
 }
 module.exports.createSession=function(req,res){
-    
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in signing in'); return}
+        if(user){
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        } else{
+            return res.redirect('back');
+        }
+        
+    })
 }
 
 
